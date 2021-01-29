6342d752c306e0cbff6f17fd1f3f92bac3c131f2
async-devil <async.devil@gmail.com>
2021-01-30 00:42:49 +0200

    Updated shell scripts

--- startdev.sh
+++ startdev.sh
@@ -1,8 +1,18 @@
-if [ ! -d "build" ]; then
-  echo "Build directory does not exists"
+#!/bin/bash
+
+echo "Rebuild?"
+
+while read ANSW; do
+  if [[ "$ANSW" == 'N' ]] || [[ "$ANSW" == 'n' ]]; then
+    break;
+  elif [[ "$ANSW" == 'Y' ]] || [[ "$ANSW" == 'y' ]]; then
     echo "Trying to compile ts"
     npx tsc
-fi
+    break;
+  else
+    echo "Unknown command"
+  fi
+done
 
 echo "Running build/index.js"
 node build/index.js
