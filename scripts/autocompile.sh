a62ba4a9bd8e35b8b00eadf34cc469d4cba622a0
async-devil <async.devil@gmail.com>
2021-02-01 00:13:14 +0200

    Updated autocompile.sh

--- scripts/autocompile.sh
+++ scripts/autocompile.sh
@@ -4,9 +4,10 @@
 target="es6"
 module="commonjs"
 outDir="build"
-watchPath="./src/*.ts"
+watchPath1="./src/**/*.ts"
+watchPath2="./src/*.ts"
 ######################
 
-echo "Starting tsc in watch mode with parametrs: -t $target -m $module --outDir $outDir --watch $watchPath"
+echo "Starting tsc in watch mode with parametrs: -t $target -m $module --outDir $outDir --watch $watchPath1 --watch $watchPath2"
 
 npx tsc -t $target -m $module --outDir $outDir --esModuleInterop true --skipLibCheck true --watch $watchPath
