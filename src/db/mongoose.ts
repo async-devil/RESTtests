db904b2492c7e623c7bc6235094e160cbcc28b56
async-devil <async.devil@gmail.com>
2021-02-01 00:13:44 +0200

    Updated mongoose settings

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -4,4 +4,5 @@ mongoose.connect(`${process.env.DB_HOST || 'mongodb://127.0.0.1:27017'}/${proces
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
+  useFindAndModify: false,
 });
