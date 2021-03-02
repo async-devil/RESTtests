ea77cc31c3845864d5bfd3cacb9ad6d6d120c28e
async-devil <async.devil@protonmail.com>
2021-03-02 23:59:53 +0200

    Added maintenance mode

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,15 @@ import TaskRoute from './routes/task.route';
 const app = express();
 const port = process.env.APP_PORT || 3000;
 
+const maintenanceMode = false;
+app.use((req: express.Request, res: express.Response, next) => {
+  if (maintenanceMode) {
+    res.status(503).send('Service unavailable');
+  } else {
+    next();
+  }
+});
+
 app.use(express.json(), UserRoute, TaskRoute);
 
 app.listen(port, () => {
