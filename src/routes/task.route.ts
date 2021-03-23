1520d903842fd0e674aab9b926e6dc4cb2e4bb29
async-devil <async.devil@protonmail.com>
2021-03-23 23:57:54 +0200

    Updated task route

--- src/routes/task.route.ts
+++ src/routes/task.route.ts
@@ -15,10 +15,18 @@ route.put('/tasks', auth, (req: Request, res: Response) => {
     .catch((err: any) => res.status(err.status).send(err.message));
 });
 
-route.get('/tasks', (req: Request, res: Response) => {
-  Method.getAllExistingModelDocuments(Task)
-    .then((task) => res.status(200).send(task))
-    .catch((err: any) => res.status(err.status).send(err.message));
+route.get('/tasks', auth, async (req: Request, res: Response) => {
+  const _id = req.params.id;
+
+  try {
+    const tasks = await Task.find({ owner: req.user._id });
+
+    if (tasks.length === 0) return res.status(404).send('There are no tasks');
+
+    res.send(tasks);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 });
 
 route.get('/tasks/:id', auth, async (req: Request, res: Response) => {
