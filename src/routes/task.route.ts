1733cc03844710c2e004ec2feeadcdf16879808a
async-devil <async.devil@protonmail.com>
2021-04-06 23:48:22 +0300

    feat: added filter, pagination and sort support

--- src/routes/task.route.ts
+++ src/routes/task.route.ts
@@ -15,9 +15,48 @@ route.put('/tasks', auth, (req: Request, res: Response) => {
     .catch((err: any) => res.status(err.status).send(err.message));
 });
 
+/*
+  ? descending|ascending
+  ! /tasks? completed=boolean & limit=number & skip=number & sortBy=propertyName:desc|asc
+  & GET /tasks?completed=true&limit=2&skip=2&sortBy=completed:desc
+  ^ des => true...false
+  ^ asc => false...true
+*/
 route.get('/tasks', auth, async (req: Request, res: Response) => {
+  const match = {} as {
+    completed: boolean;
+  };
+  if (req.query.completed) match.completed = req.query.completed === 'true';
+
+  let sort = {};
+  if (req.query.sortBy) {
+    const parts = (req.query.sortBy as string).split(':');
+    if (parts[1] === 'desc' || parts[1] === 'asc')
+      sort = new Object({ [`${parts[0]}`]: parts[1] === 'desc' ? -1 : 1 });
+    //? -1 = desc  1 = asc
+  }
+
+  const limit = () => {
+    const parsedValue = parseInt(req.query.limit as string);
+    return parsedValue ? parsedValue : 0; //? if NaN
+  };
+  const skip = () => {
+    const parsedValue = parseInt(req.query.skip as string);
+    return parsedValue ? parsedValue : 0;
+  };
+
   try {
-    await req.user.populate('tasks').execPopulate();
+    await req.user
+      .populate({
+        path: 'tasks',
+        match,
+        options: {
+          limit: limit(),
+          skip: skip(),
+          sort,
+        },
+      })
+      .execPopulate();
 
     const tasks = req.user.tasks;
     if (tasks.length === 0) return res.status(404).send('There are no tasks');
